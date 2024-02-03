6f86a0f60fdc011717d7a907fb37d0c9240e9a99
Alessandro Bustreo <bustreo27@gmail.com>
2024-02-03 13:57:24 +0100

    Base di partenza

---
+++ src/main/java/it/qbsoftware/boot/JMAPServer.java
@@ -0,0 +1,10 @@
+package it.qbsoftware.boot;
+
+/**
+ * JMAPServer
+ */
+public class JMAPServer {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/it/qbsoftware/boot/JMAPServerTest.java
@@ -0,0 +1,5 @@
+package it.qbsoftware.boot;
+
+public class JMAPServerTest {
+    
+}
