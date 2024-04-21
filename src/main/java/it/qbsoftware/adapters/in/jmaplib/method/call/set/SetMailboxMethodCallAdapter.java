59527454cc2ce861df5964133ce9529ac82285ab
raffaele <raffaele.fontana.02@gmail.com>
2024-04-21 20:21:36 +0200

    methods are no longer recursive

--- src/main/java/it/qbsoftware/adapters/in/jmaplib/method/call/set/SetMailboxMethodCallAdapter.java
+++ src/main/java/it/qbsoftware/adapters/in/jmaplib/method/call/set/SetMailboxMethodCallAdapter.java
@@ -1,10 +1,11 @@
 package it.qbsoftware.adapters.in.jmaplib.method.call.set;
 
 import java.util.Map;
-
+import it.qbsoftware.adapters.in.jmaplib.entity.MailboxAdapter;
 import it.qbsoftware.business.ports.in.jmap.entity.MailboxPort;
 import it.qbsoftware.business.ports.in.jmap.method.call.set.SetMailboxMethodCallPort;
 import rs.ltt.jmap.common.method.call.mailbox.SetMailboxMethodCall;
+import java.util.stream.Collectors;
 
 public class SetMailboxMethodCallAdapter implements SetMailboxMethodCallPort {
     SetMailboxMethodCall setMailboxMethodCall;
@@ -15,21 +16,22 @@ public class SetMailboxMethodCallAdapter implements SetMailboxMethodCallPort {
 
     @Override
     public String accountId() {
-        return this.accountId();
+        return setMailboxMethodCall.getAccountId();
     }
 
     @Override
     public String ifInState() {
-        return this.ifInState();
+        return setMailboxMethodCall.getIfInState();
     }
 
     @Override
     public Map<String, MailboxPort> getCreate() {
-        return this.getCreate();
+        return this.setMailboxMethodCall.getCreate().entrySet().stream()
+                .collect(Collectors.toMap(entry -> entry.getKey(), entry -> new MailboxAdapter(entry.getValue())));
     }
 
     @Override
     public Map<String, Map<String, Object>> getUpdate() {
-        return this.getUpdate();
+        return setMailboxMethodCall.getUpdate();
     }
 }
