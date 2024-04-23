da615a74cf4785fc776d3ac66830f755effc079e
sylvyastud <sylvya.stud@gmail.com>
2024-04-23 08:57:06 +0200

    un quasi test di echomethodcall

--- src/test/java/it/qbsoftware/business/service/EchoMethodCallServiceTest.java
+++ src/test/java/it/qbsoftware/business/service/EchoMethodCallServiceTest.java
@@ -1,29 +1,71 @@
 package it.qbsoftware.business.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.Test;
-
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
-@RunWith(MockitoJUnitRunner.class)
+import it.qbsoftware.business.ports.in.usecase.EchoMethodCallUsecase;
+import it.qbsoftware.business.services.EchoMethodCallSerivce;
+import rs.ltt.jmap.common.method.response.core.EchoMethodResponse;
+import it.qbsoftware.business.ports.in.jmap.method.call.other.EchoMethodCallPort;
+import it.qbsoftware.business.ports.in.jmap.method.response.MethodResponsePort;
+import it.qbsoftware.business.ports.in.jmap.method.response.other.EchoMethodResponseBuilderPort;
+
+@RunWith(org.mockito.junit.MockitoJUnitRunner.class)
 public class EchoMethodCallServiceTest {
+
+    @Mock
+    private MethodResponsePort methodResponsePort;
+
+    @Mock
+    private EchoMethodCallPort echoMethodCallPort;
+
+    @Mock
+    private EchoMethodResponseBuilderPort echoMethodResponseBuilderPort;
+
+    @InjectMocks
+    private EchoMethodCallSerivce echoMethodCallSerivce;
+
+
+
     @Test
     public void callEchoMethodCallService() {
-        // Call obj
-        //String payload = "TestPayload";
-        //EchoMethodCallPort mockEchoMethodCallPort = mock(EchoMethodCallPort.class);
-        //when(mockEchoMethodCallPort.payload()).thenReturn(payload);
+        String payload = "TestPayload";
+        
+        when(echoMethodCallPort.payload()).thenReturn(payload);
+        when(echoMethodResponseBuilderPort.payload(payload)).thenReturn(echoMethodResponseBuilderPort);
+        when(echoMethodResponseBuilderPort.build()).thenReturn(methodResponsePort);
 
-        // Response
-        //MethodResponsePort mockMethodResponsePort = mock(MethodResponsePort.class);
+        MethodResponsePort[] methodResponsePorts = echoMethodCallSerivce.call(echoMethodCallPort);
 
-        // Return
-        //EchoMethodResponseBuilderPort mockEchoMethodResponseBuilderPort = mock(EchoMethodResponseBuilderPort.class);
-        //when(mockEchoMethodResponseBuilderPort.payload(null)).thenReturn(mockEchoMethodResponseBuilderPort);
-        //when(mockEchoMethodResponseBuilderPort.build()).thenReturn(mockMethodResponsePort);
-
-        //EchoMethodCallUsecase echoMethodCallUsecase = new EchoMethodCallSerivce(mockEchoMethodResponseBuilderPort);
-
-        //assertEquals(echoMethodCallUsecase.call(mockEchoMethodCallPort).getClass(), EchoMethodResponse.class);
+        verify(echoMethodResponseBuilderPort).payload(echoMethodCallPort.payload());
+        assertEquals(1, methodResponsePorts.length);
+        //TODO: attendo ale per capire i tipi
+        //assertEquals(EchoMethodResponseBuilderPort.class, methodResponsePorts[0].getClass());
     }
+
+    @Test
+    public void testEchoMethodCallServiceWithEmptyPayload() {
+        String payload = "";
+        
+        when(echoMethodCallPort.payload()).thenReturn(payload);
+        when(echoMethodResponseBuilderPort.payload(payload)).thenReturn(echoMethodResponseBuilderPort);
+        when(echoMethodResponseBuilderPort.build()).thenReturn(methodResponsePort);
+
+        MethodResponsePort[] methodResponsePorts = echoMethodCallSerivce.call(echoMethodCallPort);
+
+        verify(echoMethodResponseBuilderPort).payload(echoMethodCallPort.payload());
+        assertEquals(1, methodResponsePorts.length);
+        //TODO: attendo ale per capire i tipi
+        //assertEquals(EchoMethodResponseBuilderPort.class, methodResponsePorts[0].getClass());
+    }
+
 }
+
+
