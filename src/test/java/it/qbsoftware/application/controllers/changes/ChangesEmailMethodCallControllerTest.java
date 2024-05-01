a4cb3cdf2a9867a563024adb58016dfc26a49247
Silvia <sylvya.stud@gmail.com>
2024-05-01 21:12:04 +0200

    fatto primo update

---
+++ src/test/java/it/qbsoftware/application/controllers/changes/ChangesEmailMethodCallControllerTest.java
@@ -0,0 +1,112 @@
+package it.qbsoftware.application.controllers.changes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import it.qbsoftware.adapters.in.jmaplib.method.response.changes.ChangesEmailMethodResponseAdapter;
+import it.qbsoftware.application.controllers.HandlerRequest;
+import it.qbsoftware.business.domain.exception.AccountNotFoundException;
+import it.qbsoftware.business.domain.exception.InvalidArgumentsException;
+import it.qbsoftware.business.domain.exception.changes.CannotCalculateChangesException;
+import it.qbsoftware.business.ports.in.guava.ListMultimapPort;
+import it.qbsoftware.business.ports.in.jmap.entity.ResponseInvocationPort;
+import it.qbsoftware.business.ports.in.usecase.changes.ChangesEmailMethodCallUsecase;
+import rs.ltt.jmap.common.method.MethodCall;
+import rs.ltt.jmap.common.method.MethodResponse;
+import rs.ltt.jmap.common.method.call.email.ChangesEmailMethodCall;
+import rs.ltt.jmap.common.method.error.CannotCalculateChangesMethodErrorResponse;
+import rs.ltt.jmap.common.method.error.InvalidArgumentsMethodErrorResponse;
+import rs.ltt.jmap.common.method.error.UnknownMethodMethodErrorResponse;
+import rs.ltt.jmap.common.method.response.email.ChangesEmailMethodResponse;
+
+@RunWith(org.mockito.junit.MockitoJUnitRunner.class)
+public class ChangesEmailMethodCallControllerTest {
+
+    @Mock
+    ChangesEmailMethodCallUsecase changesEmailMethodCallUsecase;
+
+    @Mock
+    ListMultimapPort<String,ResponseInvocationPort> previousResponses;
+
+    @Mock
+    ChangesEmailMethodResponseAdapter changesEmailMethodResponseAdapter;
+
+    @Mock
+    MethodCall methodCall;
+
+    @Mock
+    ChangesEmailMethodResponse changesEmailMethodResponse;
+
+    @InjectMocks
+    ChangesEmailMethodCallController changesEmailMethodCallController;
+
+
+    @Test
+    public void testHandle() throws Exception {
+        ChangesEmailMethodCall changesEmailMethodCall = new ChangesEmailMethodCall("null", "null", 1L);
+        HandlerRequest handlerRequest = new HandlerRequest(changesEmailMethodCall, previousResponses);
+
+        when(changesEmailMethodCallUsecase.call(any(),any())).thenReturn(changesEmailMethodResponseAdapter);
+        when(changesEmailMethodResponseAdapter.adaptee()).thenReturn(changesEmailMethodResponse);
+
+        MethodResponse result = changesEmailMethodCallController.handle(handlerRequest);
+
+        assertEquals(changesEmailMethodResponse, result);
+    }
+
+    
+    @Test
+    public void testHandleWithAccountNotFoundException() throws Exception {
+        ChangesEmailMethodCall changesEmailMethodCall = new ChangesEmailMethodCall("null", "null", 1L);
+        HandlerRequest handlerRequest = new HandlerRequest(changesEmailMethodCall, previousResponses);
+
+        when(changesEmailMethodCallUsecase.call(any(),any())).thenThrow(AccountNotFoundException.class);
+
+        MethodResponse result = changesEmailMethodCallController.handle(handlerRequest);
+
+        assertTrue(result instanceof InvalidArgumentsMethodErrorResponse);
+    }
+
+    @Test
+    public void testHandleWithCannotCalculateChangesException() throws Exception {
+        ChangesEmailMethodCall changesEmailMethodCall = new ChangesEmailMethodCall("null", "null", 1L);
+        HandlerRequest handlerRequest = new HandlerRequest(changesEmailMethodCall, previousResponses);
+
+        when(changesEmailMethodCallUsecase.call(any(),any())).thenThrow(CannotCalculateChangesException.class);
+
+        MethodResponse result = changesEmailMethodCallController.handle(handlerRequest);
+
+        assertTrue(result instanceof CannotCalculateChangesMethodErrorResponse);
+    }
+
+    @Test
+    public void testHandleWithInvalidArgumentsException() throws Exception {
+        ChangesEmailMethodCall changesEmailMethodCall = new ChangesEmailMethodCall("null", "null", 1L);
+        HandlerRequest handlerRequest = new HandlerRequest(changesEmailMethodCall, previousResponses);
+
+        when(changesEmailMethodCallUsecase.call(any(),any())).thenThrow(InvalidArgumentsException.class);
+
+        MethodResponse result = changesEmailMethodCallController.handle(handlerRequest);
+
+        assertTrue(result instanceof InvalidArgumentsMethodErrorResponse);
+    }
+
+    @Test
+    public void testHandleSuper() throws Exception {
+
+        HandlerRequest handlerRequest = new HandlerRequest(methodCall, previousResponses);
+
+        MethodResponse result = changesEmailMethodCallController.handle(handlerRequest);
+
+        assertTrue(result instanceof UnknownMethodMethodErrorResponse);
+    }
+
+
+}
--- src/test/java/it/qbsoftware/business/domain/methodcall/process/set/update/StandardUpdateEmailTest.java
+++ src/test/java/it/qbsoftware/business/domain/methodcall/process/set/update/StandardUpdateEmailTest.java
@@ -3,16 +3,10 @@ package it.qbsoftware.business.domain.methodcall.process.set.update;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
@@ -24,6 +18,7 @@ import it.qbsoftware.business.domain.entity.changes.AccountState;
 import it.qbsoftware.business.domain.entity.changes.tracker.EmailChangesTracker;
 import it.qbsoftware.business.domain.entity.changes.tracker.MailboxChangesTracker;
 import it.qbsoftware.business.domain.exception.AccountNotFoundException;
+import it.qbsoftware.business.domain.exception.set.SetNotFoundException;
 import it.qbsoftware.business.domain.util.get.CreationIdResolverPort;
 import it.qbsoftware.business.ports.in.guava.ListMultimapPort;
 import it.qbsoftware.business.ports.in.jmap.entity.EmailBuilderPort;
@@ -81,7 +76,6 @@ public class StandardUpdateEmailTest {
     @InjectMocks
     StandardUpdateEmail standardUpdateEmail;
 
-
     @Test
     public void testUpdateWithNullIdsToUpdate() throws AccountNotFoundException {     
         
@@ -93,80 +87,6 @@ public class StandardUpdateEmailTest {
         assertTrue(result.notUpdated().isEmpty());
         assertNotNull(result);
     }
-
-    /*@Test
-    public void testUpdateWithKeywords() throws Exception {     
-        
-        Map<String, Object> mapObj = new HashMap<>();
-        mapObj.put("keywords/qualcosa", true);
-        //mapObj.put("mailboxIds", true);
-        Map<String, Map<String, Object>> mapEmailToUpdate = new HashMap<>();
-        mapEmailToUpdate.put("key", mapObj);
- 
-        EmailPort emailPort2 = mock(EmailPort.class);
-        EmailBuilderPort emailBuilderPort2 = mock(EmailBuilderPort.class);
-
-        when(setEmailMethodCallPort.getUpdate()).thenReturn(mapEmailToUpdate);
-        when(emailRepository.retriveOne(any())).thenReturn(emailPort);
-        when(emailPort.toBuilder()).thenReturn(emailBuilderPort);
-
-        
-        when(setEmailMethodCallPort.accountId()).thenReturn("accountId");
-        when(accountStateRepository.retrive(anyString())).thenReturn(accountState);
-        when(accountState.increaseState()).thenReturn(accountState);
-        when(accountState.state()).thenReturn("state");
-
-        when(emailChangesTrackerRepository.retrive(any())).thenReturn(emailChangesTracker);
-
-        when(emailBuilderPort.clearMailboxIds()).thenReturn(emailBuilderPort);
-        when(emailBuilderPort.reset()).thenReturn(emailBuilderPort);
-        //when(emailBuilderPort.id(anyString())).thenReturn(emailBuilderPort);
-        //when(emailBuilderPort.blobId(anyString())).thenReturn(emailBuilderPort);
-        //when(emailBuilderPort.threadId(any())).thenReturn(emailBuilderPort);
-        //when(emailBuilderPort.size(anyLong())).thenReturn(emailBuilderPort);
-        //when(emailBuilderPort.mailboxIds(anyMap())).thenReturn(emailBuilderPort);
-        //when(emailBuilderPort.receivedAt(any())).thenReturn(emailBuilderPort);
-        //when(emailBuilderPort.attachment(any())).thenReturn(emailBuilderPort);
-        //when(emailBuilderPort.clearMailboxIds()).thenReturn(emailBuilderPort);
-        //when(emailBuilderPort.clearAttachments()).thenReturn(emailBuilderPort);
-
-        when(emailPort.toBuilder()).thenReturn(emailBuilderPort);
-
-        when(emailBuilderPort.keywords(any())).thenReturn(emailBuilderPort);
-        when(emailBuilderPort.build()).thenReturn(emailPort);
-        when(emailBuilderPort2.build()).thenReturn(emailPort);
-
-        doNothing().when(emailRepository).overwrite(emailPort);
-
-        UpdatedResult<EmailPort> result = standardUpdateEmail.update(setEmailMethodCallPort, previousResponses);
-
-        assertNotNull(result);
-    }
-
-    @Test
-    public void testUpdateWithMailboxIds() throws Exception {     
-
-        Map<String, Object> mapObj = new HashMap<>();
-        mapObj.put("mailboxIds/qualcosa", true);
-        Map<String, Map<String, Object>> mapEmailToUpdate = new HashMap<>();
-        mapEmailToUpdate.put("/keywords", mapObj);
-
-        when(setEmailMethodCallPort.getUpdate()).thenReturn(mapEmailToUpdate);
-        when(emailRepository.retriveOne(any())).thenReturn(emailPort);
-        when(emailPort.toBuilder()).thenReturn(emailBuilderPort);
-
-        when(setEmailMethodCallPort.accountId()).thenReturn("accountId");
-        when(accountStateRepository.retrive(anyString())).thenReturn(accountState);
-        when(accountState.increaseState()).thenReturn(accountState);
-        when(accountState.state()).thenReturn("state");
-
-        when(mailboxChangesTrackerRepository.retrive(any())).thenReturn(mailboxChangesTracker);
-
-
-        UpdatedResult<EmailPort> result = standardUpdateEmail.update(setEmailMethodCallPort, previousResponses);
-
-        assertNotNull(result);
-    }*/
 
     @Test
     public void testUpdateWithMailboxMap() throws Exception {     
@@ -187,7 +107,6 @@ public class StandardUpdateEmailTest {
         when(accountState.state()).thenReturn("state");
 
         when(mailboxChangesTrackerRepository.retrive(any())).thenReturn(mailboxChangesTracker);
-
 
         UpdatedResult<EmailPort> result = standardUpdateEmail.update(setEmailMethodCallPort, previousResponses);
 
@@ -213,27 +132,20 @@ public class StandardUpdateEmailTest {
         assertNotNull(result);
     }
 
-    /*@Test
+    @Test
     public void testUpdateWithSetNotFoundException() throws Exception {     
 
         Map<String, Object> mapObj = new HashMap<>();
         mapObj.put("/keywords", true);
         Map<String, Map<String, Object>> mapEmailToUpdate = new HashMap<>();
         mapEmailToUpdate.put("/keywords", mapObj);
+        SetNotFoundException setNotFoundException = new SetNotFoundException();
 
         when(setEmailMethodCallPort.getUpdate()).thenReturn(mapEmailToUpdate);
-        when(emailRepository.retriveOne(any())).thenReturn(emailPort);
-        when(emailPort.toBuilder()).thenReturn(emailBuilderPort);
-
+        when(emailRepository.retriveOne(any())).thenThrow(setNotFoundException);
         when(setEmailMethodCallPort.accountId()).thenReturn("accountId");
-        //when(accountStateRepository.retrive(anyString())).thenReturn(accountState);
-        //when(accountState.increaseState()).thenReturn(accountState);
-        //when(accountState.state()).thenReturn("state");
-
-        //when(mailboxChangesTrackerRepository.retrive(any())).thenReturn(mailboxChangesTracker);
-
         UpdatedResult<EmailPort> result = standardUpdateEmail.update(setEmailMethodCallPort, previousResponses);
 
         assertNotNull(result);
-    }*/
+    }
 }
